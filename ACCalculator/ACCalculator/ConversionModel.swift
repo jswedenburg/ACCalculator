2db79155b617c5912398a251417c437d8b755749
jswedenburg <jswedenburg@gmail.com>
2016-09-25 23:13:33 -0600

    Adjusted Constraints

--- ACCalculator/ACCalculator/ConversionModel.swift
+++ ACCalculator/ACCalculator/ConversionModel.swift
@@ -9,16 +9,13 @@
 import Foundation
 
 private let constant: Float = 3.412
-
 private let coolingLoad: Float = 12.0
 
 struct KwPerTon {
-    
     let kwPerTon: Float
     var eer: Float {
         return coolingLoad / kwPerTon
     }
-    
     var cop: Float {
         return coolingLoad / kwPerTon / constant
     }
@@ -26,11 +23,9 @@ struct KwPerTon {
 
 struct Eer {
     let eer: Float
-    
     var kwPerTon: Float {
        return coolingLoad / eer
     }
-    
     var cop: Float {
         return eer / constant
     }
@@ -38,11 +33,9 @@ struct Eer {
 
 struct Cop {
     let cop: Float
-    
     var kwPerTon: Float {
         return 12 / (cop * constant)
     }
-    
     var eer: Float {
         return cop * constant
     }
